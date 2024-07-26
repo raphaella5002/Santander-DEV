4b967712e508e7d6ec0095ab7bfee807b6270a13
Raphaella Martins <raphaella.karolline@gmail.com>
2024-07-26 13:39:04 -0300

    first commit

---
+++ src/test/java/me/dio/Santander/DEV/SantanderDevApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander.DEV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
